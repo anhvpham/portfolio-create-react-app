6b7b18db2bb40a433a950172d01ea78cb4422b48
anhphm23@gmail.com <Aplangara-0118>
2020-09-11 17:40:33 -0700

    fixed minor errors

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,4 +1,4 @@
-import REact from 'react';
+import React from 'react';
 import Layout from '../../components/Layout';
 import { SectionTitle, Paragraph} from '../../styles';
 import { WorkItem, WorkTitle, JobTitle } from './styles';
