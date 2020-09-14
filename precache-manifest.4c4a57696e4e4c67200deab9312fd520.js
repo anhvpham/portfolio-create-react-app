2cb259c1517ee81126132d92d0b4ede032899f47
anhphm23@gmail.com <Aplangara-0118>
2020-09-14 16:11:09 -0700

    Updates

---
+++ precache-manifest.4c4a57696e4e4c67200deab9312fd520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232cc0e8efcc84ccbe1ed65c6e3da8d4",
+    "url": "/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "e26fe7d0d1cede536374",
+    "url": "/portfolio-create-react-app/static/css/main.2dee4063.chunk.css"
+  },
+  {
+    "revision": "47c9b6424665375ee631",
+    "url": "/portfolio-create-react-app/static/js/2.a3ff2cb8.chunk.js"
+  },
+  {
+    "revision": "e26fe7d0d1cede536374",
+    "url": "/portfolio-create-react-app/static/js/main.20c0b8cc.chunk.js"
+  },
+  {
+    "revision": "999046f5fad93a8fea10",
+    "url": "/portfolio-create-react-app/static/js/runtime-main.36396636.js"
+  }
+]);
